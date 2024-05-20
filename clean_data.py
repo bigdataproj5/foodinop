24ee1b9c3de71432c8315069a40e5e49a52363e0
mamkat <katarzyna.piegat@gmail.com>
2024-05-20 08:46:49 +0200

    Correction for quotation

--- clean_data.py
+++ clean_data.py
@@ -8,25 +8,30 @@ import csv
 
 raw_data_folder = "C:\\Studia\\Big_Data\\Projekt\\Data_Sets\\Data-ready\\"
 
+# Read csv file to the data frame
 df_RegionsAccordingWorldBank = pd.read_csv(raw_data_folder + "world-regions-according-to-the-world-bank.csv",encoding='latin-1', dtype=str)
 df_continents = pd.read_csv(raw_data_folder + "continents-according-to-our-world-in-data.csv",encoding='latin-1', dtype=str)
 df_FoodBalanceSheets = pd.read_csv(raw_data_folder + "FoodBalanceSheets_E_All_Data_(Normalized).csv",encoding='latin-1', dtype=str)
-df_FoodBalanceSheetsHistoric = pd.read_csv("C:\\Studia\\Big_Data\\Projekt\\Data_Sets\\Data-ready\\FoodBalanceSheetsHistoric_E_All_Data_(Normalized).csv",encoding='latin-1', dtype=str)
+df_FoodBalanceSheetsHistoric = pd.read_csv(raw_data_folder + "FoodBalanceSheetsHistoric_E_All_Data_(Normalized).csv",encoding='latin-1', dtype=str)
 df_LifeExpectancy = pd.read_csv(raw_data_folder + "life-expectancy.csv",encoding='latin-1', dtype=str)
 df_WhoRegions = pd.read_csv(raw_data_folder + "who-regions.csv",encoding='latin-1', dtype=str)
 df_RegionsUnitedNations = pd.read_csv(raw_data_folder + "world-regions-sdg-united-nations.csv",encoding='latin-1', dtype=str)
 df_FoodBalanceSheetsEAreaCodes = pd.read_csv(raw_data_folder + "copy_FoodBalanceSheets_E_AreaCodes.csv",encoding='latin-1', dtype=str)
 
-dt_string = datetime.now().replace(microsecond=0)
 
-def clean_data(df, df_name):
-
+def clean_data(df, df_name,raw_data_name):
+# arguments description: 
+# df - to put datframe name as datframe type
+# df_name - to put the dataframe name as as string 
+    
+    dt_string = datetime.now().replace(microsecond=0)
     # prepare variables to open file
     data_folder = "C:\\Studia\\Big_Data\\Projekt\\Data_Sets\\Data-ready\\"
     file_to_open = data_folder + df_name + ".log" 
+    
     # open file to log progress
     with open(file_to_open, "a") as logfile:
-    
+        
         # start cleaning data from NULLs
         print("############################################################################",file=logfile)
         print("### CLEAN DATA FOR ", df_name, " FILE date: ", dt_string, file=logfile)
@@ -81,41 +86,28 @@ def clean_data(df, df_name):
         print("### REMOVE WRONG APOSTROFS ", df_name, " FILE date: ", dt_string, file=logfile)
         print("##############################################################################", file=logfile)
         print(" ",file=logfile)
-        print("Columns with Apostrofs in dataframes", file=logfile)
-        print("df_FoodBalanceSheets, columns: [Area Code (M49)], [Item Code (FBS)]", file=logfile)
-        print("df_FoodBalanceSheetsHistoric columns: [Area Code (M49)], [Item Code (FBS)]", file=logfile)
-        print("df_FoodBalanceSheetsEAreaCodes columns: [M49 Code]", file=logfile)
         
-        df_FoodBalanceSheets['Area Code (M49)'] = df_FoodBalanceSheets['Area Code (M49)'].str.replace("'","")
-        df_FoodBalanceSheets['Item Code (FBS)'] = df_FoodBalanceSheets['Item Code (FBS)'].str.replace("'","")
-        df_FoodBalanceSheetsHistoric['Area Code (M49)'] = df_FoodBalanceSheetsHistoric['Area Code (M49)'].str.replace("'","")
-        df_FoodBalanceSheetsEAreaCodes['M49 Code'] = df_FoodBalanceSheetsEAreaCodes['M49 Code'].str.replace("'","")
+        if df_name == "df_FoodBalanceSheets":
+            df['Area Code (M49)'] = df['Area Code (M49)'].str.replace("'","")
+            df['Item Code (FBS)'] = df['Item Code (FBS)'].str.replace("'","")
+            print("Remove apostrofs from " + df_name + " columns: [Area Code (M49)], [Item Code (FBS)]", file=logfile)
+        elif df_name == "df_FoodBalanceSheetsHistoric":
+            df['Area Code (M49)'] = df['Area Code (M49)'].str.replace("'","")
+            print("Remove apostrofs from " + df_name + " columns: [Area Code (M49)], [Item Code (FBS)]", file=logfile)
+        elif df_name == "df_FoodBalanceSheetsEAreaCodes":
+            df['M49 Code'] = df['M49 Code'].str.replace("'","")
+            print("Remove apostrofs from " + df_name + " columns: [M49 Code]", file=logfile)
+            
+        df.to_csv(raw_data_folder + raw_data_name + "_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
+        print(df.head())
         
     return None
  
-clean_data(df_RegionsAccordingWorldBank, 'df_RegionsAccordingWorldBank')
-clean_data(df_continents, 'df_continents')
-clean_data(df_FoodBalanceSheets, 'df_FoodBalanceSheets')
-clean_data(df_FoodBalanceSheetsHistoric, 'df_FoodBalanceSheetsHistoric')
-clean_data(df_LifeExpectancy, 'df_LifeExpectancy')
-clean_data(df_WhoRegions, 'df_WhoRegions')
-clean_data(df_RegionsUnitedNations, 'df_RegionsUnitedNations')
-clean_data(df_FoodBalanceSheetsEAreaCodes, 'df_FoodBalanceSheetsEAreaCodes')
-
-df_RegionsAccordingWorldBank.to_csv(raw_data_folder + "world-regions-according-to-the-world-bank_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_continents.to_csv(raw_data_folder + "continents-according-to-our-world-in-data_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_FoodBalanceSheets.to_csv(raw_data_folder + "FoodBalanceSheets_E_All_Data_(Normalized)_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_FoodBalanceSheetsHistoric.to_csv(raw_data_folder + "FoodBalanceSheetsHistoric_E_All_Data_(Normalized)_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_LifeExpectancy.to_csv(raw_data_folder + "life-expectancy_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_WhoRegions.to_csv(raw_data_folder + "who-regions_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_RegionsUnitedNations.to_csv(raw_data_folder + "world-regions-sdg-united-nations_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-df_FoodBalanceSheetsEAreaCodes.to_csv(raw_data_folder + "copy_FoodBalanceSheets_E_AreaCodes_CLEAN.csv", sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False, encoding='utf-8')
-
-print(df_RegionsAccordingWorldBank.head())
-print(df_continents.head())
-print(df_FoodBalanceSheets.head())
-print(df_FoodBalanceSheetsHistoric.head())
-print(df_LifeExpectancy.head())
-print(df_WhoRegions.head())
-print(df_RegionsUnitedNations.head())
-print(df_FoodBalanceSheetsEAreaCodes.head())
+clean_data(df_RegionsAccordingWorldBank, "df_RegionsAccordingWorldBank","world-regions-according-to-the-world-bank.csv")
+clean_data(df_continents, "df_continents", "continents-according-to-our-world-in-data.csv")
+clean_data(df_FoodBalanceSheets, "df_FoodBalanceSheets", "FoodBalanceSheets_E_All_Data_(Normalized).csv")
+clean_data(df_FoodBalanceSheetsHistoric, "df_FoodBalanceSheetsHistoric", "FoodBalanceSheetsHistoric_E_All_Data_(Normalized).csv")
+clean_data(df_LifeExpectancy, "df_LifeExpectancy", "life-expectancy.csv")
+clean_data(df_WhoRegions, "df_WhoRegions", "who-regions.csv")
+clean_data(df_RegionsUnitedNations, "df_RegionsUnitedNations", "world-regions-sdg-united-nations.csv")
+clean_data(df_FoodBalanceSheetsEAreaCodes, "df_FoodBalanceSheetsEAreaCodes", "copy_FoodBalanceSheets_E_AreaCodes.csv")
